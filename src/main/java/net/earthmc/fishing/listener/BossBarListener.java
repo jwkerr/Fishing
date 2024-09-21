73ebebde66a4317fb16bcc63c86c08026c38eb48
jwkerr <49851457+jwkerr@users.noreply.github.com>
2024-09-21 16:26:12 +1000

    Show bossbar when connecting

--- src/main/java/net/earthmc/fishing/listener/BossBarListener.java
+++ src/main/java/net/earthmc/fishing/listener/BossBarListener.java
@@ -1,12 +1,16 @@
 package net.earthmc.fishing.listener;
 
+import com.palmergames.bukkit.towny.TownyAPI;
 import com.palmergames.bukkit.towny.event.player.PlayerEntersIntoTownBorderEvent;
 import com.palmergames.bukkit.towny.event.player.PlayerExitsFromTownBorderEvent;
+import com.palmergames.bukkit.towny.object.Town;
 import net.earthmc.fishing.api.EventManager;
 import net.earthmc.fishing.object.FishingEvent;
 import net.kyori.adventure.bossbar.BossBar;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 
 public class BossBarListener implements Listener {
 
@@ -30,5 +34,18 @@ public class BossBarListener implements Listener {
 
         BossBar bossBar = fe.getBossBar();
         event.getPlayer().hideBossBar(bossBar);
+    }
+
+    @EventHandler
+    public void onPlayerJoinInTown(PlayerJoinEvent event) {
+        EventManager em = EventManager.getInstance();
+        FishingEvent fe = em.getActiveEvent();
+        if (fe == null) return;
+
+        Player player = event.getPlayer();
+        Town town = TownyAPI.getInstance().getTown(player.getLocation());
+
+        BossBar bossBar = fe.getBossBar();
+        if (fe.getTown().equals(town)) player.showBossBar(bossBar);
     }
 }
